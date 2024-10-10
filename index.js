673c8d78f40630a45dfa6382f5fbe5cb36e67450
kien <dp1.1a2kien@gmail.com>
2024-10-11 00:12:36 +0700

    Xây dựng layout chung

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const RouterAdmin = require("./routers/admin/index.router.js");
 require("dotenv").config();
 
 const database = require("./config/database.js");
-
+const systenConfig = require("./config/system.js");
 database.connect();
 const port = process.env.PORT;
 app.set("views", "./views");
@@ -14,6 +14,7 @@ app.set("view engine", "pug");
 app.use(express.static("public")); //nhúng file tĩnh
 Router(app);
 RouterAdmin(app);
+app.locals.prefixAdmin = systenConfig.prefixAdmin;
 app.listen(port, (req, res) => {
   console.log(`Example app listening on port ${port}`);
 });
