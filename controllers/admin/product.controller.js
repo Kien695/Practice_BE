e7db90c085917ead0717164d7b83c97b093144e5
kien <dp1.1a2kien@gmail.com>
2024-10-11 19:15:31 +0700

    Tính năng lọc theo trạng thái

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,12 +1,45 @@
 const Product = require("../../models/product.model");
 //[get]admin/products
 module.exports.index = async (req, res) => {
-  const Products = await Product.find({
-    deleted: "false",
-  });
-  console.log(Products);
+  let filterStatus = [
+    {
+      name: "Tất cả",
+      status: "",
+      class: "",
+    },
+    {
+      name: "Hoạt động",
+      status: "active",
+      class: "",
+    },
+    {
+      name: "Dừng hoạt động",
+      status: "inactive",
+      class: "",
+    },
+  ];
+  if (req.query.status) {
+    const index = filterStatus.findIndex(
+      (item) => item.status == req.query.status
+    );
+    filterStatus[index].class = "active";
+  } else {
+    const index = filterStatus.findIndex((item) => item.status == "");
+    filterStatus[index].class = "active";
+  }
+  let find = {
+    deleted: false,
+    //status: req.query.status,
+  };
+  if (req.query.status) {
+    find.status = req.query.status; //thêm status vào find
+  }
+
+  const Products = await Product.find(find);
+
   res.render("admin/pages/products/index", {
     pageTitle: "Trang sản phẩm",
     products: Products,
+    filterStatus: filterStatus,
   });
 };
