7b79859db85f353ade1bbf552a65ffdcb42af6c5
kien <dp1.1a2kien@gmail.com>
2024-11-01 08:58:09 +0700

    logout

--- routers/admin/auth.router.js
+++ routers/admin/auth.router.js
@@ -4,4 +4,5 @@ const authController = require("../../controllers/admin/auth.controller");
 const validate = require("../../validates/admin/auth.validate");
 router.get("/login", authController.login);
 router.post("/login", validate.loginPost, authController.loginPost);
+router.get("/logout", authController.logout);
 module.exports = router;
