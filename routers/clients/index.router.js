58f87e0ceeac7756a095dece08347306c12a9cfe
kien <dp1.1a2kien@gmail.com>
2024-11-03 20:32:21 +0700

    hiện thị menu

--- routers/clients/index.router.js
+++ routers/clients/index.router.js
@@ -1,6 +1,8 @@
 const homeRouter = require("./home.router.js");
 const productsRouter = require("./products.router.js");
+const categoryMiddleware = require("../../middlewares/client/category.middleware.js");
 module.exports = (app) => {
+  app.use(categoryMiddleware.category);
   app.use("/", homeRouter);
   app.use("/products", productsRouter);
 };
