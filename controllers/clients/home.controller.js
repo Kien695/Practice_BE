7a3714d4a74692244a6c031e9260c5ff4c6aec30
kien <dp1.1a2kien@gmail.com>
2024-11-06 18:59:29 +0700

    hiện thị nổi bật

--- controllers/clients/home.controller.js
+++ controllers/clients/home.controller.js
@@ -1,6 +1,16 @@
+const Product = require("../../models/product.model");
+const productsHelper = require("../../helpers/products");
 //[get] /
 module.exports.index = async (req, res) => {
+  //lấy ra sản phẩm nổi bật
+  const productFeatured = await Product.find({
+    featured: "1",
+    deleted: "false",
+    status: "active",
+  }).limit(6);
+  const newProducts = productsHelper.priceNewProducts(productFeatured);
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
+    productFeatured: newProducts,
   });
 };
