0b4b02438dad1cc004523876e35641ab4931e611
kien <dp1.1a2kien@gmail.com>
2024-11-06 21:24:33 +0700

    ds sp moi

--- controllers/clients/home.controller.js
+++ controllers/clients/home.controller.js
@@ -8,9 +8,18 @@ module.exports.index = async (req, res) => {
     deleted: "false",
     status: "active",
   }).limit(6);
-  const newProducts = productsHelper.priceNewProducts(productFeatured);
+  const newProductsFeatured = productsHelper.priceNewProducts(productFeatured);
+  //hiển thị danh sách sản phẩm mới nhất
+  const productsNew = await Product.find({
+    deleted: "false",
+    status: "active",
+  })
+    .sort({ position: "desc" })
+    .limit(6);
+  const newProductsNew = productsHelper.priceNewProducts(productsNew);
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
-    productFeatured: newProducts,
+    productFeatured: newProductsFeatured,
+    productsNew: newProductsNew,
   });
 };
