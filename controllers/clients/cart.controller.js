7b49e8d28c33208df60ddfd6eacdbcbd8ec34ce3
kien <dp1.1a2kien@gmail.com>
2024-11-08 23:17:02 +0700

    xóa sp tỏng giỏ hàng

--- controllers/clients/cart.controller.js
+++ controllers/clients/cart.controller.js
@@ -73,3 +73,18 @@ module.exports.addPost = async (req, res) => {
   req.flash("success", "Thêm sản phẩm vào giỏ hàng thành công!");
   res.redirect("back");
 };
+//[get] /cart/delete/:productId
+module.exports.delete = async (req, res) => {
+  const cartId = req.cookies.cartId;
+  const productId = req.params.productId;
+  await Cart.updateOne(
+    {
+      _id: cartId,
+    },
+    {
+      $pull: { products: { product_id: productId } },
+    }
+  );
+  req.flash("success", "Xóa sản phẩm thành công");
+  res.redirect("back");
+};
