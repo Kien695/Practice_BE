8fb35511faf8b42256b7f3e834e7f78185a72ee2
kien <dp1.1a2kien@gmail.com>
2024-11-08 22:26:03 +0700

    trang giỏ hàng

--- routers/clients/cart.router.js
+++ routers/clients/cart.router.js
@@ -2,4 +2,5 @@ const express = require("express");
 const router = express.Router();
 const cartController = require("../../controllers/clients/cart.controller");
 router.post("/add/:productId", cartController.addPost);
+router.get("/", cartController.index);
 module.exports = router;
