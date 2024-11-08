79d377b76e8ab5d596df1cee5b3a48ce3cf296d9
kien <dp1.1a2kien@gmail.com>
2024-11-08 10:22:27 +0700

    kq tìm kiếm

--- routers/clients/index.router.js
+++ routers/clients/index.router.js
@@ -1,8 +1,10 @@
 const homeRouter = require("./home.router.js");
 const productsRouter = require("./products.router.js");
+const searchRouter = require("./search.router.js");
 const categoryMiddleware = require("../../middlewares/client/category.middleware.js");
 module.exports = (app) => {
   app.use(categoryMiddleware.category);
   app.use("/", homeRouter);
   app.use("/products", productsRouter);
+  app.use("/search", searchRouter);
 };
