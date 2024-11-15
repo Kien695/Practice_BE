0c556e10494f95ee4c0b71769abc8e7d90953a5b
kien <dp1.1a2kien@gmail.com>
2024-11-15 16:47:29 +0700

    hiện thị

--- controllers/clients/chat.controller.js
+++ controllers/clients/chat.controller.js
@@ -1,11 +1,34 @@
+const Chat = require("../../models/chat.model");
+const User = require("../../models/user.model");
 //[get] /checkout
 module.exports.index = async (req, res) => {
+  const userId = res.locals.user.id;
   //socket.id
-  _io.on("connection", (socket) => {
-    console.log("a user connected", socket.id);
+  _io.once("connection", (socket) => {
+    socket.on("CLIENT_SEND_MASSAGE", async (content) => {
+      //lưu vào database
+      const chat = new Chat({
+        user_id: userId,
+        content: content,
+      });
+      await chat.save();
+    });
   });
+  //lấy data ra giao diện
+  const chats = await Chat.find({
+    deleted: false,
+  });
+
+  for (const chat of chats) {
+    const infoUser = await User.findOne({
+      _id: chat.user_id,
+    }).select("fullName");
+    chat.infoUser = infoUser;
+  }
+
   //end socket.io
   res.render("client/pages/chat/index", {
     pageTitle: "Chat",
+    chats: chats,
   });
 };
