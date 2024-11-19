d0901b1208dff38a99003c3d54dc2e248b53ef6d
kien <dp1.1a2kien@gmail.com>
2024-11-19 20:41:19 +0700

    chấp nhận

--- sockets/client/users.socket.js
+++ sockets/client/users.socket.js
@@ -103,5 +103,54 @@ module.exports = (res) => {
         );
       }
     });
+    //chức năng chấp nhận kết bạn
+    socket.on("CLIENT_ACCEPT_FRIEND", async (userId) => {
+      const myUserId = res.locals.user.id;
+      //thêm {user_id;room_chat_id} của A vào friendList của B
+      //xóa Id của A vào acceptFriends của B
+      const exitIdAinB = await User.findOne({
+        _id: myUserId,
+        acceptFriends: userId,
+      });
+      if (exitIdAinB) {
+        await User.updateOne(
+          {
+            _id: myUserId,
+          },
+          {
+            $push: {
+              FriendList: {
+                user_id: userId,
+                room_chat_id: "",
+              },
+            },
+
+            $pull: { acceptFriends: userId },
+          }
+        );
+      }
+      //thêm {user_id;room_chat_id} của B vào friendList của A
+      //xóa id của B vào requestFriends của A
+      const exitIdBinA = await User.findOne({
+        _id: userId,
+        requestFriends: myUserId,
+      });
+      if (exitIdBinA) {
+        await User.updateOne(
+          {
+            _id: userId,
+          },
+          {
+            $push: {
+              FriendList: {
+                user_id: myUserId,
+                room_chat_id: "",
+              },
+            },
+            $pull: { requestFriends: myUserId },
+          }
+        );
+      }
+    });
   });
 };
